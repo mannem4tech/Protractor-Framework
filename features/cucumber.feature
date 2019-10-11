d952ef07cdf842a4d4d4c89722b2f06566d88bbd
Hari Mannem <harimannem@Haris-15-MacBook-Pro.local>
2019-10-11 12:08:47 -0400

    inLine&outLine

--- features/cucumber.feature
+++ features/cucumber.feature
@@ -1,8 +1,31 @@
 @Tag
 Feature: Testing
 
-This is optional section, we can add details about this feature file.
+    This is optional section, we can add details about this feature file.
 
-Scenario: One scenario for protractor testing
-    Given I will naviagte to the "https://www.protractortest.org" url
-    Then I verify the content on the page
+    @123
+    Scenario: One scenario for protractor testing
+        Given I will naviagte to the "https://www.protractortest.org" url
+        Then I verify the content on the page
+
+
+    @inLine
+    Scenario: One scenario for inline protractor testing
+        Given I will naviagte to the "https://www.protractortest.org" url
+        Then I should able to handle all "Record" and "Type"
+            | Record  | Type     |
+            | Badges  | Contact  |
+            | Badges1 | Contact2 |
+
+    @outLine
+    Scenario Outline: Scenario fpr outline testing
+        Given I will naviagte to the "https://www.protractortest.org" url
+        Then I navigate to "page" and verify the context
+        Examples:
+            | page                           |
+            | Setting Up Protractor          |
+            | Setting Up the Selenium Server |
+            | Setting Up the Browser         |
+            | Choosing a Framework           |
+
+
--- steps/step-definition.steps.ts
+++ steps/step-definition.steps.ts
@@ -1,5 +1,5 @@
 import { browser, protractor, element, by } from 'protractor';
-import { Given, Then } from 'cucumber';
+import { Given, Then, TableDefinition } from 'cucumber';
 import chai = require("chai");
 const until = protractor.ExpectedConditions;
 const expect = chai.expect;
@@ -19,4 +19,23 @@ Then(/^I verify the content on the page$/, async function (): Promise<any> {
     //await browser.executeScript("arguments[0].style.WebkitAnimationName='from {left: 0px;} to {left: 200px;}'", element(by.className('protractor-logo')));
     let screenShot = await browser.takeScreenshot();
     this.attach(screenShot, "image/png");
+});
+
+
+Then('I should able to handle all "([^"]*)" and "([^"]*)"',
+    async function (argRecord: string, argType: string, addTable: TableDefinition): Promise<void> {
+        let dataTable: any = addTable.hashes();
+        for (let index in dataTable) {
+            if (dataTable.hasOwnProperty(index)) {
+                let [record, type] = Object.values(dataTable[index]);
+                console.log(record);
+                console.log(type);
+            }
+        }
+    });
+
+Then('I navigate to {string} and verify the context', async function (string): Promise<void> {
+    // Write code here that turns the phrase above into concrete actions
+    console.log(string);
+    return Promise.resolve();
 });
